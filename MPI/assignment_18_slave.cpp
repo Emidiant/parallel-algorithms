c92a153a1740b0f416cb062b97f9985e456457ad
Julia <emidiant1999@gmail.com>
2021-12-22 04:14:17 +0300

    add assignment_18

---
+++ MPI/assignment_18_slave.cpp
@@ -0,0 +1,13 @@
+//#include "mpi.h"
+#import "/usr/local/Cellar/open-mpi/4.1.2/include/mpi.h"
+
+int main(int argc, char **argv) {
+    int rank;
+    MPI_Comm intercomm;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_get_parent(&intercomm);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Send(&rank, 1, MPI_INT, 0, rank, intercomm);
+    MPI_Finalize();
+    return 0;
+}
