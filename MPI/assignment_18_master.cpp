536859a938917ae664aa1e6d7b896a22b564ec90
Julia <emidiant1999@gmail.com>
2021-12-22 04:24:49 +0300

    add assignment_18

--- MPI/assignment_18_master.cpp
+++ MPI/assignment_18_master.cpp
@@ -4,17 +4,20 @@
 #import "/usr/local/Cellar/open-mpi/4.1.2/include/mpi.h"
 
 int main(int argc, char **argv) {
-    int size, rank1, rank2;
+    int size, rank1, rank2, number_processes;
     MPI_Status status;
     MPI_Comm intercomm;
     char slave[14] = "slave_18.o";
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-    MPI_Comm_spawn(slave, MPI_ARGV_NULL, 2, MPI_INFO_NULL, 0, MPI_COMM_SELF, &intercomm, MPI_ERRCODES_IGNORE);
+    MPI_Comm_spawn(slave, MPI_ARGV_NULL, 3, MPI_INFO_NULL, 0, MPI_COMM_SELF, &intercomm, MPI_ERRCODES_IGNORE);
     MPI_Recv(&rank1, 1, MPI_INT, 0, 0, intercomm, &status);
     MPI_Recv(&rank2, 1, MPI_INT, 1, 1, intercomm, &status);
 
     printf("Slaves %d and %d are working\n", rank1, rank2);
+
+    MPI_Recv(&number_processes, 1, MPI_INT, 2, 2, intercomm, &status);
+    printf("Number of processes: %d\n", number_processes);
     MPI_Finalize();
     return 0;
 }
--- MPI/assignment_18_slave.cpp
+++ MPI/assignment_18_slave.cpp
@@ -2,12 +2,16 @@
 #import "/usr/local/Cellar/open-mpi/4.1.2/include/mpi.h"
 
 int main(int argc, char **argv) {
-    int rank;
+    int rank, size;
     MPI_Comm intercomm;
     MPI_Init(&argc, &argv);
     MPI_Comm_get_parent(&intercomm);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Send(&rank, 1, MPI_INT, 0, rank, intercomm);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    if (rank == 2)
+        MPI_Send(&size, 1, MPI_INT, 0, rank, intercomm);
+    else
+        MPI_Send(&rank, 1, MPI_INT, 0, rank, intercomm);
     MPI_Finalize();
     return 0;
 }
