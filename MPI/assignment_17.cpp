07215d2421cf45c40eb45def9bd12a7df352fa09
Julia <emidiant1999@gmail.com>
2021-12-22 03:39:39 +0300

    add assignment_17.cpp

--- MPI/assignment_17.cpp
+++ MPI/assignment_17.cpp
@@ -12,7 +12,7 @@ int main(int argc, char **argv) {
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     for (i = 0; i < 10; i++) {
-        a[i] = rank + 1;
+        a[i] = rank + 1.0;
         if (rank == 0) b[i] = 'a';
         else b[i] = 'b';
     }
